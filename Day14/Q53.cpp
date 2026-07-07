f50455132821037eba1adf0991169f2d86920704
shreyaa023 <shreyagupta022006@gmail.com>
2026-07-07 21:38:06 +0530

    Implement linear search in Q53.cpp

---
+++ Day14/Q53.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    int position = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            position = i;
+            break;
+        }
+    }
+
+    if (position != -1)
+        cout << "Element found at position " << position + 1 << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
